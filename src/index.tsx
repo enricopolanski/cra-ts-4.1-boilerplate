ac804f649cfd709b9263a75f3a2d9ecc4d301680
enricopolanski <enricopolanski@gmail.com>
2020-11-22 12:37:08 +0100

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App: React.FC = () => <h1>Boilerplate test</h1>;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
